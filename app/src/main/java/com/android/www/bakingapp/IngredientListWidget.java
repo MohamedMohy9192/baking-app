f24eb333cea1a714540c75dae33ea149dd7c7201
Mohamed <mohamedmohy9192@gmail.com>
2018-05-02 04:18:05 +0200

    Add Widget provider and its xml file and its layout.

---
+++ app/src/main/java/com/android/www/bakingapp/IngredientListWidget.java
@@ -0,0 +1,52 @@
+package com.android.www.bakingapp;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+
+/**
+ * Implementation of App Widget functionality.
+ */
+public class IngredientListWidget extends AppWidgetProvider {
+
+    private static final int NO_FLAG = 0;
+    private static final int PENDING_INTENT_CODE = 1;
+
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
+                                int appWidgetId) {
+
+
+        // Construct the RemoteViews object
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.ingredient_list_widget);
+
+        Intent intent = new Intent(context, MainActivity.class);
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, PENDING_INTENT_CODE, intent, NO_FLAG);
+
+        views.setOnClickPendingIntent(R.id.appwidget_image, pendingIntent);
+        // Instruct the widget manager to update the widget
+        appWidgetManager.updateAppWidget(appWidgetId, views);
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        // There may be multiple widgets active, so update all of them
+        for (int appWidgetId : appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId);
+        }
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+}
+
