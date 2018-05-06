f09bec621e64ec04bc1959be1af8cbfbb2a983cc
Mohamed <mohamedmohy9192@gmail.com>
2018-05-06 20:31:09 +0200

    Add RecyclerView click test

---
+++ app/src/androidTest/java/com/android/www/bakingapp/MainRecyclerViewTest.java
@@ -0,0 +1,35 @@
+package com.android.www.bakingapp;
+
+import android.support.test.espresso.Espresso;
+import android.support.test.espresso.action.ViewActions;
+import android.support.test.espresso.assertion.ViewAssertions;
+import android.support.test.espresso.contrib.RecyclerViewActions;
+import android.support.test.espresso.matcher.ViewMatchers;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * Created by OWNER on 5/6/2018.
+ */
+
+@RunWith(AndroidJUnit4.class)
+public class MainRecyclerViewTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mainActivityActivityTestRule =
+            new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void scrollRecyclerView_checkRecipeNameMatched() {
+        Espresso.onView(ViewMatchers.withId(R.id.rv_main_recipe))
+                .perform(RecyclerViewActions.actionOnItem(
+                        ViewMatchers.hasDescendant(ViewMatchers.withText("Nutella Pie")),
+                        ViewActions.click()));
+    }
+
+
+}
